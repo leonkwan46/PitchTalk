adf1b8625f90ae8dcf2f01e1155741ba51b9239f
sdc <sdc>
2024-07-17 11:44:09 +0800

    Initial stage re-build project

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from 'react-native'
+import ContainerLoginSignup from '../components/shared/loginSignup/ContainerLoginSignup';
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Test</Text>
+    </View>
+  );
+}
+
+export default Index
